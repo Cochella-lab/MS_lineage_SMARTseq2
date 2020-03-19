d690041b37a7881b171f6453731c69d518388ef7
jiwang <jingkui.wang@imp.ac.at>
2020-03-19 11:16:47 +0100

    initialize new script for Murray s scRNA-seq data processing

---
+++ scripts_analysis/processing_published_dataSet.R
@@ -0,0 +1,42 @@
+##########################################################################
+##########################################################################
+# Project: Aleks' MS lineage project
+# Script purpose: processing John Murray's scRNA-seq 
+# Usage example: 
+# Author: Jingkui Wang (jingkui.wang@imp.ac.at)
+# Date of creation: Thu Mar 19 11:10:31 2020
+##########################################################################
+##########################################################################
+source.my.script <- function(name.of.function){
+  tryCatch(path <- rstudioapi::getSourceEditorContext()$path,
+           error = function(e){
+             install.packages("rstudioapi")
+             path <-  rstudioapi::getSourceEditorContext()$path})
+  source.path <- sub(basename(path), "", path)
+  source(paste0(source.path,name.of.function))
+}
+
+## set up the paths for the data and results
+tryCatch(path <- rstudioapi::getSourceEditorContext()$path, 
+         error = function(e){
+           install.packages("rstudioapi")
+           path <-  rstudioapi::getSourceEditorContext()$path})
+source.path <- sub(basename(path), "", path)
+
+
+user <- "results_jiwang/"
+setwd(paste0("../", user))
+
+version.DATA = 'scRNA_Murray_2019'
+version.analysis =  paste0(version.DATA, '_20200319')
+dataDir = paste0("../data/gene_counts/")
+resDir = paste0("results/", version.analysis)
+tabDir = paste0("results/", version.analysis, "/tables/")
+
+RdataDir = paste0("results/", version.analysis, "/Rdata/")
+if(!dir.exists("results/")){dir.create("results/")}
+if(!dir.exists(resDir)){dir.create(resDir)}
+if(!dir.exists(tabDir)){dir.create(tabDir)}
+if(!dir.exists(RdataDir)){dir.create(RdataDir)}
+
+source.my.script('scATAC_functions.R')
